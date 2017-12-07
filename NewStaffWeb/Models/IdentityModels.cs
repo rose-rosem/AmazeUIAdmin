fd994f83d39a2838be3afb0ab252dc734c2389ce
rose-rosem <825021872@qq.com>
2017-12-07 20:12:36 +0800

    添加新的内部管理网站
    添加新的内部管理网站

---
+++ NewStaffWeb/Models/IdentityModels.cs
@@ -0,0 +1,44 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace NewStaffWeb.Models
+{
+    // 可以通过向 ApplicationUser 类添加更多属性来为用户添加配置文件数据。若要了解详细信息，请访问 http://go.microsoft.com/fwlink/?LinkID=317594。
+    public class ApplicationUser : IdentityUser
+    {
+        [Display(Name = "真实姓名"), Required]
+        public string FullName { get; set; }
+
+        [Display(Name = "年龄")]
+        public int Age { get; set; }
+
+        [Display(Name = "城市")]
+        public string City { get; set; }
+
+
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
+        {
+            // 请注意，authenticationType 必须与 CookieAuthenticationOptions.AuthenticationType 中定义的相应项匹配
+            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+            // 在此处添加自定义用户声明
+            return userIdentity;
+        }
+    }
+
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection")
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
